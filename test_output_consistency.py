e62566ff5a78a38fe634462f2af4db7c7bf0698b
Sebastian Szymanski <sebastian.szymanski@semantive.com>
2021-04-06 15:45:52 +0200

    Added tests for Lite variants.

--- test_output_consistency.py
+++ test_output_consistency.py
@@ -3,15 +3,15 @@ import unittest
 import numpy as np
 import tensorflow as tf
 
-TOLERANCE = 1e-3
-
-from official_eff_net import (
+from efficientnet import (
     EfficientNetB0,
     EfficientNetB1,
     EfficientNetB2,
     EfficientNetB3,
     EfficientNetB4
 )
+
+TOLERANCE = 1e-3
 
 
 class OutputConsistencyTestSuite:
@@ -61,6 +61,36 @@ class TestEfficientNetB4Output(unittest.TestCase, OutputConsistencyTestSuite):
     model = EfficientNetB4()
     input_shape = (380, 380)
     original_model_output = "assets/original_outputs/b4_output.npy"
+
+
+class TestEfficientNetB0LiteOutput(unittest.TestCase, OutputConsistencyTestSuite):
+    model = EfficientNetB0(lite=True, weights="weights/efficient_net_lite_b0.h5")
+    input_shape = (224, 224)
+    original_model_output = "assets/original_outputs/b0_lite_output_224.npy"
+
+
+class TestEfficientNetB1LiteOutput(unittest.TestCase, OutputConsistencyTestSuite):
+    model = EfficientNetB1(lite=True, weights="weights/efficient_net_lite_b1.h5")
+    input_shape = (240, 240)
+    original_model_output = "assets/original_outputs/b1_lite_output_240.npy"
+
+
+class TestEfficientNetB2LiteOutput(unittest.TestCase, OutputConsistencyTestSuite):
+    model = EfficientNetB2(lite=True, weights="weights/efficient_net_lite_b2.h5")
+    input_shape = (260, 260)
+    original_model_output = "assets/original_outputs/b2_lite_output_260.npy"
+
+
+class TestEfficientNetB3LiteOutput(unittest.TestCase, OutputConsistencyTestSuite):
+    model = EfficientNetB3(lite=True, weights="weights/efficient_net_lite_b3.h5")
+    input_shape = (280, 280)
+    original_model_output = "assets/original_outputs/b3_lite_output_280.npy"
+
+
+class TestEfficientNetB4LiteOutput(unittest.TestCase, OutputConsistencyTestSuite):
+    model = EfficientNetB4(lite=True, weights="weights/efficient_net_lite_b4.h5")
+    input_shape = (300, 300)
+    original_model_output = "assets/original_outputs/b4_lite_output_300.npy"
 
 
 if __name__ == '__main__':
