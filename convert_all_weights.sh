5490aa86ab7159942a98ab15d0f6f74fbd0a97c9
Sebastian Szymanski <sebastian.szymanski@semantive.com>
2021-04-06 16:49:59 +0200

    Added weights download and conversion code.

--- convert_all_weights.sh
+++ convert_all_weights.sh
@@ -1,29 +1,29 @@
 python efficientnet_weight_update_util.py \
   --model b0 \
-  --ckpt weights/efficientnet-lite0/model.ckpt \
+  --ckpt weights/original_weights/efficientnet-lite0/model.ckpt \
   --lite \
   --o weights/efficient_net_lite_b0.h5
 
 python efficientnet_weight_update_util.py \
   --model b1 \
-  --ckpt weights/efficientnet-lite1/model.ckpt \
+  --ckpt weights/original_weights/efficientnet-lite1/model.ckpt \
   --lite \
   --o weights/efficient_net_lite_b1.h5
 
 python efficientnet_weight_update_util.py \
   --model b2 \
-  --ckpt weights/efficientnet-lite2/model.ckpt \
+  --ckpt weights/original_weights/efficientnet-lite2/model.ckpt \
   --lite \
   --o weights/efficient_net_lite_b2.h5
 
 python efficientnet_weight_update_util.py \
   --model b3 \
-  --ckpt weights/efficientnet-lite3/model.ckpt \
+  --ckpt weights/original_weights/efficientnet-lite3/model.ckpt \
   --lite \
   --o weights/efficient_net_lite_b3.h5
 
 python efficientnet_weight_update_util.py \
   --model b4 \
-  --ckpt weights/efficientnet-lite4/model.ckpt \
+  --ckpt weights/original_weights/efficientnet-lite4/model.ckpt \
   --lite \
   --o weights/efficient_net_lite_b4.h5
