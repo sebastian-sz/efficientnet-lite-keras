957946078a2ecee62d1cb2cfb9e7edf2c9e1376b
sebastian-sz <mocart15@gmail.com>
2021-06-28 08:00:52 +0200

    Fixed download error due to missing directories. Added docker docs. Fixed package redownload due to TF package naming conventions.

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
+from pkg_resources import DistributionNotFound, get_distribution
 from setuptools import setup
 
 with open("README.md", encoding="utf-8") as f:
     long_description = "\n" + f.read()
+
+
+def _package_exists(name: str) -> bool:
+    """Check whether package is present in the system."""
+    try:
+        get_distribution(name)
+    except DistributionNotFound:
+        return False
+    else:
+        return True
+
+
+def _get_tensorflow_requirement():
+    """Avoid re-download and misdetection of package."""
+    lower = 2.2
+    upper = 2.6
+
+    if _package_exists("tensorflow-cpu"):
+        return [f"tensorflow-cpu>={lower},<{upper}"]
+    elif _package_exists("tensorflow-gpu"):
+        return [f"tensorflow-gpu>={lower},<{upper}"]
+    else:
+        return [f"tensorflow>={lower},<{upper}"]
+
 
 setup(
     name="efficientnet-lite-keras",
@@ -20,5 +45,5 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     packages=["efficientnet_lite"],
-    install_requires=["tensorflow>=2.2,<2.6"],
+    install_requires=_get_tensorflow_requirement(),
 )
