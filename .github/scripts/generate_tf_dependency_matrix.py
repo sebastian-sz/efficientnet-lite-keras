a499677d4c4a7952af4fe9af1375b7e0ae0c116b
sebastian-sz <mocart15@gmail.com>
2021-11-05 09:21:29 +0100

    Added dynamic test matrix generation.

---
+++ .github/scripts/generate_tf_dependency_matrix.py
@@ -0,0 +1,51 @@
+import json
+from argparse import ArgumentParser
+
+
+def _parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-r",
+        "--requirements",
+        default="requirements.txt",
+        help="Path to requirements.txt file.",
+    )
+    return parser.parse_args()
+
+
+def main():
+    """Print Tensorflow version matrix to test."""
+    args = _parse_arguments()
+
+    content = _read_file(args.requirements)
+    tf_requirement = _get_tf_requirement(content)
+
+    for r in (">=", ",<"):
+        tf_requirement = tf_requirement.replace(r, ",")
+    _, lower, upper = tf_requirement.split(",")
+
+    lower_minor = int(lower.split(".")[-1])
+    upper_minor = int(upper.split(".")[-1])
+
+    # Skip versions below 2.4 - too old and hard to maintain
+    lower_minor = 4 if lower_minor < 4 else lower_minor
+    matrix = [f"2.{minor}" for minor in range(lower_minor, upper_minor)]
+
+    print(json.dumps(matrix))
+
+
+def _read_file(path):
+    with open(path, "r") as f:
+        content = f.readlines()
+    return content
+
+
+def _get_tf_requirement(content):
+    requirement = [x for x in content if x.startswith("tensorflow>=")][0]
+    if requirement.endswith("\n"):
+        requirement = requirement.rstrip("\n")
+    return requirement
+
+
+if __name__ == "__main__":
+    main()
